271309d9c6897bce389f6bcad17ca3495e183dbb
mirziyo <mirziyo>
2025-01-30 16:04:50 +0500

    implement user interface functionality with input filtering, item deletion, and loading state

--- js/mode.js
+++ js/mode.js
@@ -1,6 +1,13 @@
 const body = document.querySelector("body");
 const darkBtn = document.getElementById("dark-btn");
 const lightBtn = document.getElementById("light-btn");
+const modeLocal = localStorage.getItem("mode");
+
+if (modeLocal) {
+  body.classList.add("dark-mode");
+  darkBtn.classList.toggle("hidden");
+  lightBtn.classList.toggle("hidden");
+}
 
 const toggleModeBtin = () => {
   darkBtn.classList.toggle("hidden");
--- js/request.js
+++ js/request.js
@@ -1,5 +1,47 @@
 // api
-const API = 'https://randomuser.me/api/?results=9'
+const API = "https://randomuser.me/api/?results=9";
 
 // for leader
-const overlay = document.getElementById('overlay')
+const overlay = document.getElementById("overlay");
+
+const loaderToggle = (toggle) => {
+  if (toggle) {
+    overlay.classList.remove("hidden");
+  } else {
+    overlay.classList.add("hidden");
+  }
+};
+
+const getDate = (resource) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState < 4) {
+        loaderToggle(true);
+      } else if (request.readyState == 4 && request.status == 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data.results);
+        loaderToggle(false);
+      } else if (request.request == 4) {
+        reject("Error!!!");
+        loaderToggle(false);
+      }
+    });
+
+    request.open("GET", resource);
+    request.send();
+  });
+};
+
+const reload = () => {
+  getDate(API)
+    .then((data) => {
+      updateUI(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+document.addEventListener("DOMContentLoaded", reload);
